64c1ad9b1960247706a4e4999dea0738dbde6eac
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-21 13:22:06 +0200

    goit-js-hw-07 task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryElements = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+// Формуємо розмітку галереї на основі масиву даних
+const createGalleryMarkup = galleryItems => {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    })
+    .join('');
+};
+
+// Рендеримо розмітку галереї
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.5,
+    captionDelay: 250,
+    captionsData: 'alt',
+    fadeSpeed: 250,
+    rtl: true,
+});
